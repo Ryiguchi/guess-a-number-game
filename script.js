c05862f4b31e13c81d4c93e5d5af9c0bf5a204e8
Ryan Iguchi <ryan.iguchi1@gmail.com>
2022-10-19 12:50:14 +0200

    New features and styling

--- script.js
+++ script.js
@@ -2,33 +2,54 @@
 
 const textGuess = document.querySelector(".guess-label");
 const textMessage = document.querySelector(".message");
+const textSecNum = document.querySelector(".secret-number");
 
 const list = document.querySelector("ul");
 
 const input = document.querySelector("input");
 
+const btnPlay = document.querySelector(".play-btn");
 const btnGuess = document.querySelector(".guess-btn");
 const btnReset = document.querySelector(".reset-btn");
 
 const body = document.querySelector("body");
 const h1 = document.querySelector("h1");
+const pressPlayBox = document.querySelector(".press-play-wrapper");
+const secretNumBox = document.querySelector(".secret-number-wrapper");
+const gameSection = document.querySelector(".game-section");
 
-let turnNum = 1;
-let secretNum;
+let secretNum, turnNum;
+const numMin = 0;
+const numMax = 100;
+const maxTurns = 5;
 
 // Functions //////////////////////////////
 
-// Returns a random number between 'min' and 'max'
+///////// Initializes game
+const init = function () {
+  pressPlayBox.classList.add("hidden");
+  secretNumBox.classList.remove("hidden");
+  gameSection.classList.remove("hidden");
+
+  secretNum = getRandomNum(numMin, numMax);
+  console.log(secretNum);
+  turnNum = 1;
+  input.value = "";
+  h1.textContent = "Number Guessing Game";
+  textMessage.textContent = "";
+  list.innerHTML = "";
+  textSecNum.textContent = "?";
+  // Styling
+  body.style.backgroundColor = "#6853ab";
+  h1.style.color = "#e0c95e";
+};
+
+///////// Returns a random number between 'min' and 'max'
 const getRandomNum = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-// Gets number from input field
-const getGuess = function () {
-  return Math.floor(input.value);
-};
-
-// displays message and lists the guess
+////////// Displays message and lists the guess
 const displayGuess = function (guess, secretNum) {
   input.value = "";
   const highLow = guess < secretNum ? "low" : "high";
@@ -41,14 +62,7 @@ const displayGuess = function (guess, secretNum) {
   } than ${guess}, try again ...`;
 };
 
-// Notifys that an invalid number was entered
-const displayNaN = function (guess) {
-  const text = `<li>Guess ${turnNum}:   invalid</li>`;
-  list.insertAdjacentHTML("beforeend", text);
-
-  textMessage.textContent = `You entered an invalid number, try again ...`;
-};
-
+/////// Displsays win or lose
 const gameEnd = function (guess, result) {
   // Styling
   body.style.backgroundColor = "#e0c95e";
@@ -63,22 +77,17 @@ const gameEnd = function (guess, result) {
     h1.textContent = "😢 You lose!!";
     textMessage.textContent = `Sorry!! You're out of guesses!!`;
   }
+  textSecNum.textContent = secretNum;
 };
 
 // Main functionality
 const playGame = function () {
-  // Get a random number if it's the first turn
-
-  if (turnNum === 1) {
-    secretNum = getRandomNum(0, 100);
-  }
-
   // Get the players guess
-  const curGuess = getGuess();
+  const curGuess = Math.floor(input.value);
 
   // Check if number
   if (isNaN(curGuess) || curGuess < 0 || curGuess > 100) {
-    displayNaN(curGuess);
+    textMessage.textContent = `You entered an invalid number, try again ...`;
     return;
   }
 
@@ -86,7 +95,7 @@ const playGame = function () {
   if (curGuess === secretNum) {
     gameEnd(curGuess, "winner");
     return;
-  } else if (turnNum === 5) {
+  } else if (turnNum === maxTurns) {
     gameEnd(curGuess, "loser");
     return;
   }
@@ -96,18 +105,14 @@ const playGame = function () {
   turnNum++;
 };
 
-// Event Handlers //////////////////////////
+// Event Listeners //////////////////////////
+
+btnPlay.addEventListener("click", init);
+
+btnReset.addEventListener("click", init);
 
 btnGuess.addEventListener("click", playGame);
 
 input.addEventListener("keydown", function (e) {
-  console.log(e);
   if (e.key === "Enter") playGame();
-});
-
-btnReset.addEventListener("click", function () {
-  turnNum = 1;
-  input.value = "";
-
-  location.reload();
 });
